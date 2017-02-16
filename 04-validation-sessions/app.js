92f55edcb234e4580b70deee13c3383b5a457ee6
Jun <cygoh@umich.edu>
2017-02-16 01:51:47 -0800

    form validation done

--- 04-validation-sessions/app.js
+++ 04-validation-sessions/app.js
@@ -15,7 +15,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout:: 'layout', layoutsDir: __dirname + '/views/layouts/'}))
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- 04-validation-sessions/routes/index.js
+++ 04-validation-sessions/routes/index.js
@@ -4,14 +4,28 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	// errors: req.session.errors will be empty at the beginning
-  res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
   // clear the errors after we send to the front-end
   req.session.erros = null;
+  req.session.success = null;
 });
 
 router.post('/submit', function(req, res, next) {
-	// Check validity 
-	
+	// Check validity, 
+	// first param is the name of an input field, 
+	// the second param is the error msg
+	// a lot of built-in validators
+	req.check('email', 'Invalid email address').isEmail();
+	req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+	let errors = req.validationErrors();
+	if (errors) {
+		req.session.errors =  errors;
+		req.session.success = false;
+	} else {
+		req.session.success = true;
+	}
+	res.redirect('/');
 })
 
 module.exports = router;
